feb68dc53f1e26d961843a4635aa7ecbe0bedaa2
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2019-12-04 14:09:43 +0100

    scripts

--- scripts/data.sh
+++ scripts/data.sh
@@ -7,7 +7,7 @@ APP="dariah-contrib"
 HOST_TEST="tclarin11.dans.knaw.nl"
 HOST_PROD="clarin11.dans.knaw.nl"
 
-if [ "$HOSTNAME" == "$HOST_TEST"  | "$HOSTNAME" == "$HOST_PROD" ]; then
+if [ "$HOSTNAME" == "$HOST_TEST" || "$HOSTNAME" == "$HOST_PROD" ]; then
     dataroot="~/dirkr/dariah-backups"
 else
     dataroot=~/Documents/DANS/projects/has/testdatabackups
