b777ce1f355780057b8de4aa851b5713b5747fa0
Dirk Roorda <dirk.roorda@dans.knaw.nl>
2019-12-16 13:51:06 +0100

    authentication debugging

--- server/control/auth.py
+++ server/control/auth.py
@@ -316,7 +316,7 @@ class Auth:
             if DEBUG_LOGIN:
                 serverprint(f"LOGIN: No shibboleth session found:")
                 serverprint(f"ENV = {env}")
-                for (k, v) in env:
+                for (k, v) in env.items():
                     serverprint(f"LOGIN: ENV[{k}] = {v}")
                 serverprint(f"LOGIN: authentication failed")
 
